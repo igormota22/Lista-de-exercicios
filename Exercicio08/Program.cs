c7bc1e5868fdfd5bca9a78685692e70481ef9819
Igor Mota <igormello399@gmail.com>
2026-03-12 16:41:50 -0300

    Exercicio 8 adicionado

---
+++ Exercicio08/Program.cs
@@ -0,0 +1,29 @@
+﻿//8. Crie um programa para verificar se um número é primo.
+
+System.Console.Write("Digite um número:");
+int numero = Convert.ToInt32(Console.ReadLine());
+
+int resto = numero % 2;
+
+if (numero <= 1)
+{
+    System.Console.WriteLine($"{numero} não é primo");
+}
+else if (numero == 2)
+{
+     System.Console.WriteLine($"{numero} é primo");
+}
+else if(resto == 0)
+{
+     System.Console.WriteLine($"{numero} não é primo");
+}
+
+for (int i = 3; i * i <= numero; i += 2)
+{
+    if (numero % i == 0)
+    {
+         System.Console.WriteLine($"{numero} não é primo");
+    }
+}
+   
+
