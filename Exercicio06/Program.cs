2747baa91043123d0329f29666424ec67cf65fd2
Igor Mota <igormello399@gmail.com>
2026-03-12 15:43:12 -0300

    Exercicio 6 adicionado

---
+++ Exercicio06/Program.cs
@@ -0,0 +1,17 @@
+﻿//6. Crie um programa para calcular a média harmônica das notas de um Aluno
+
+System.Console.Write("Informe a nota 1:");
+double n1 = Convert.ToDouble(Console.ReadLine());
+
+System.Console.Write("Informe a nota 2:");
+double n2 = Convert.ToDouble(Console.ReadLine());
+
+System.Console.Write("Informe a nota 3:");
+double n3 = Convert.ToDouble(Console.ReadLine());
+
+double reciprocros = (1/n1) + (1/n2) + (1/n3);
+
+double mediaH = 3/reciprocros;
+
+System.Console.WriteLine($"A média harmonica é de: {mediaH.ToString("F2")}");
+
