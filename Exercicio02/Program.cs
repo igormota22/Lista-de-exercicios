4c9c6b60ecd96b79bbd747381542573839f52ebb
Igor Mota <igormello399@gmail.com>
2026-03-12 14:32:20 -0300

    Adicionado exercicio 2

---
+++ Exercicio02/Program.cs
@@ -0,0 +1,13 @@
+﻿//2. Crie um programa para calcular o volume de um Cilindro
+
+System.Console.Write("Digite o raio do cilindro: ");
+decimal raio = Convert.ToDecimal(Console.ReadLine());
+
+System.Console.Write("Digite a altura do cilindro: ");
+decimal altura = Convert.ToDecimal(Console.ReadLine());
+
+decimal areaBase = 3.14m * (raio * raio);
+
+decimal volume = areaBase * altura ;
+
+System.Console.WriteLine($"O volume do cilindro é de: {volume.ToString("F2")}");
