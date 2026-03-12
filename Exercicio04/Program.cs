c76e2be97a62f544906394f76e418574b26257b9
Igor Mota <igormello399@gmail.com>
2026-03-12 14:53:42 -0300

    Adicionado exercicio 4

---
+++ Exercicio04/Program.cs
@@ -0,0 +1,8 @@
+﻿//4. Crie um programa para converter a temperatura da escala Celsius para a escala Fahrenheit
+
+System.Console.Write("Informe a temperatura em Fahrenheit:");
+double f = Convert.ToDouble(Console.ReadLine());
+
+double c = (f - 32) * (5 / 9);
+
+System.Console.WriteLine($"A temperatura de {f} Fahrenheit é de {c} em graus Célsius ");
