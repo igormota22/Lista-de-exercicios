5ff47004118e7101c8df4731646870fa857c464d
Igor Mota <igormello399@gmail.com>
2026-03-13 15:17:31 -0300

    Exercicio 23 adicionado

---
+++ Exercicio23/Program.cs
@@ -0,0 +1,24 @@
+﻿// 23. Escreva um programa que imprima os números de 1 a 100 em ordem crescente, substituindo os números
+//múltiplos de 3 pela palavra "Fizz" e os múltiplos de 5 pela palavra "Buzz". Para números que são múltiplos de
+//ambos, use "FizzBuzz".
+
+for (int i = 1; i <= 100;  i++)
+{
+   if (i % 3 == 0 && i % 5 == 0)
+    {
+        System.Console.WriteLine("FizzBuzz");
+    }
+    else if (i % 3 == 0)
+    {
+        System.Console.WriteLine("Fizz");
+    }
+    else if (i % 5 == 0)
+    {
+        System.Console.WriteLine("Buzz");
+    }
+    else
+    {
+        System.Console.WriteLine(i);
+    }
+
+}
