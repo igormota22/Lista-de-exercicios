66612ba36b3014745dc3df9f094bf8b385393c19
Igor Mota <igormello399@gmail.com>
2026-03-13 01:07:20 -0300

    Exercicio 16 adicionado

---
+++ Exercicio16/Program.cs
@@ -0,0 +1,15 @@
+﻿// 16. Faça um algoritmo para receber um número qualquer e informar na tela se é par ou ímpar.
+
+System.Console.Write("Informe um número:");
+double numero = Convert.ToDouble(Console.ReadLine());
+
+double resto = numero % 2;
+
+if (resto == 0)
+{
+    System.Console.WriteLine("É PAR");
+}
+else
+{
+    System.Console.WriteLine("É IMPAR");
+}
