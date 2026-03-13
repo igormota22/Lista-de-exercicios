0b5bc2f48eca7d42f676bdb187aea4d767c0cbf6
Igor Mota <igormello399@gmail.com>
2026-03-13 01:17:27 -0300

    Exercicio 17 adicionado

---
+++ Exercicio17/Program.cs
@@ -0,0 +1,22 @@
+﻿//17. Faça um algoritmo que leia dois valores inteiros A e B, se os valores forem iguais deverá se somar os dois, caso
+//contrário multiplique A por B. Ao final de qualquer um dos cálculos deve-se atribuir o resultado para uma
+//variável C e mostrar seu conteúdo na tela.
+
+System.Console.Write("Informe o valor de A:");
+int a = Convert.ToInt32(Console.ReadLine());
+
+System.Console.Write("Informe o valor de B:");
+int b = Convert.ToInt32(Console.ReadLine());
+
+int c;
+
+if (a == b)
+{
+     c = a + b;
+}
+else
+{
+    c = a * b;
+}
+
+System.Console.WriteLine($"O valor de C é de: {c}");
